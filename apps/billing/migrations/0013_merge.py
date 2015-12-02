5537184c89d9b2dca7f13d965f6d047b386dabcc
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2015-12-02 18:38:51 +0100

    Merge conflicting migrations

---
+++ apps/billing/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('billing', '0011_alter_order_purchase'),
+        ('billing', '0012_auto_20151122_2255'),
+    ]
+
+    operations = [
+    ]
