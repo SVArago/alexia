6b54a9dcbf52a7928b99594071ef0ddb5d29625a
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2016-10-11 21:46:54 +0200

    Update Django to 1.10 along with dependencies

--- settings/__init__.py
+++ settings/__init__.py
@@ -12,11 +12,13 @@ except ImportError:
 # Debug toolbar
 if DEBUG:
     # Enable debug toolbar if DEBUG is enabled.
-
     def show_toolbar(request):
         return True
 
     INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE_CLASSES.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+    # Explicitly enable debug toolbar for anyone, not just internal IPs
     DEBUG_TOOLBAR_CONFIG = {
         'SHOW_TOOLBAR_CALLBACK': 'settings.show_toolbar'
     }
