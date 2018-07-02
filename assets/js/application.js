9b01bebf813a29606c423f13f98ab700137352d6
Jesse Bakker <jesse.bakker@arago.utwente.nl>
2018-07-02 18:41:04 +0000

    Add comment to bartender availabilities

--- assets/js/application.js
+++ assets/js/application.js
@@ -28,6 +28,32 @@ $(function () {
         }, "text");
     });
 
+    // Bind the event id to the modal and set the existing comment
+    $('#comment_modal').on('show.bs.modal', function (event) {
+	var modal = $(this);
+	var button = $(event.relatedTarget);
+	var comment = button.data('comment');
+	var event_id = button.data('event-id');
+	modal.data('event-id', event_id);
+	modal.find('.modal-body input').val(comment);
+    });
+
+    // When the modal shows, focus the text input field, so that a user
+    // can start typing immediately
+    $('#comment_modal').on('shown.bs.modal', function () {
+	$(this).find('.modal-body input').focus();
+    });
+
+    $('#comment_modal #save').on('click', function (event) {
+	var modal = $(this).parents('#comment_modal');
+	$.post('/scheduling/ajax/bartender_availability_comment/', {
+	    event_id: modal.data('event-id'),
+	    comment: modal.find('.modal-body input').val(),
+	    csrfmiddlewaretoken: getCookie('csrftoken')
+	});
+	modal.modal('hide');
+    });
+
     $('.timeinput').timepicker({
         defaultTime: false,
         showMeridian: false
